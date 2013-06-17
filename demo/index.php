f4b4292627545c4e99119e8c411aeea16f178fff
Rich Nicholls <pixelbyter404@googlemail.com>
2013-06-17 12:32:32 +0200

    Update index.php

--- demo/index.php
+++ demo/index.php
@@ -3,11 +3,11 @@
 	//if you require in your header and footer you can use this variable as a flag not to; ?ajax=true added by jquery.popup.js to links to be opened as popups when requested via ajax
 	$show_header_and_footer = isset($_GET['ajax']) ? true : false;
 	
-	//show page with list of links to popups
-	$show_list_page = (isset($_GET['ajax']) && $show_popup_page) ? false : true; 
-	
 	//show popup page content; in your implementation you can use the id to load different content for each popup link
 	$show_popup_page = isset($_GET['id']) ? true : false;
+	
+	//show page with list of links to popups
+	$show_list_page = (isset($_GET['ajax']) && $show_popup_page) ? false : true; 
 	
 	//show header/footer
 	if($show_header_and_footer) { ?>
